9f613750f9e2958b41cd10936bcb66f09dc7bbba
itbgouveia <itbgouveia@gmail.com>
2020-12-13 13:24:21 -0300

    13-12-20

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { Container} from './styles';
+import React, { useMemo} from 'react';
+import Toggle from '../Toggle';
+import { Container,Profile, Welcome, UserName} from './styles';
+import emojis from '../../utils/emojis';
 
 const MainHeader: React.FC = () =>{
+    const emoji = useMemo(()=>{
+        const indice = Math.floor(Math.random() * 4);
+        return emojis[indice];
+    },[]);
     return(
         <Container>
-            <h1>MainHeader</h1>
+            <Toggle/>
+            <Profile>
+            <Welcome>Olá,{emoji}</Welcome>
+            <UserName>Itamar Gouveia</UserName>
+            </Profile>
         </Container>
     );
 }
